53777afa0a4019a3770ad8304923b6937f573596
emmadavids <davidsek88@gmail.com>
2022-09-29 17:56:55 +0100

    no commit for a while...

--- script.js
+++ script.js
@@ -3,36 +3,68 @@ const X = "X"
 const O = "O"
 
 const GameBoard = (() => {
+    let isXTurn = true
     let gameOver = false
+    // const checkGameOver = () = {
+        
+    // } 
     let gameArr = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]]
      return {
         gameArr
      };
+   
    })();
  
 //player turn state should be on game board function
 //game should be checked to see if its over after every move 
 //
 
-const Player = (name) => {
-    let isXTurn = true
-    const makeMove = (e) => {
-        const id = e.srcElement.id.slice(2, e.srcElement.id.length)
-        console.log(id)
-        const box = document.getElementById(`b-${id}`)
-        if (isXTurn) {
-            box.textContent = X }
-        else {
-            box.textContent = O }
+const Player = (name, piece) => {
+    const getPiece = () => piece;
+  
+        //     GameBoard.isXTurn = false;
+        // }
+       
         
-        isXTurn = false;
-    }
-    return {makeMove};
+
+        // if (GameBoard.isXTurn) {
+        //     GameBoard.gameArr[i][d] = X
+        //     box.textContent = X 
+        //     GameBoard.isXTurn = false;
+        // }
+        // else {
+        //     GameBoard.gameArr[i][d] = O
+        //     box.textContent = O 
+        //     GameBoard.isXTurn = true; 
+        //     console.log(GameBoard.gameArr)
+        // }
+
+    return {getPiece};
   };  
 
+const emma = Player('emma', X)
+const mart = Player('martin', O)
 
-const emma = Player('emma')
-
+let player = emma 
+//player is emma 
+  
+  function makeMove(e, player) {
+    console.log("playpiece:" + player.getPiece())
+    const id = e.srcElement.id.slice(2, e.srcElement.id.length)
+    const box = document.getElementById(`b-${id}`)
+    const i = id.slice(1, 2)
+    const d = id.slice(4, 5)
+    GameBoard.gameArr[i][d] = player.getPiece()
+    box.textContent = player.getPiece() 
+    if (GameBoard.isXTurn) {
+        GameBoard.isXTurn = false; //
+        player = mart;
+    }
+    else {
+        player = emma
+        GameBoard.isXTurn = true;
+}
+  }
 
 function addGrid() {
 for (let i = 0; i < GameBoard.gameArr.length; i ++) {
@@ -41,12 +73,12 @@ for (let i = 0; i < GameBoard.gameArr.length; i ++) {
         container.appendChild(row);
         
     for (let j=0; j < GameBoard.gameArr[i].length; j++) { 
-        let box = document.createElement("div");
+        const box = document.createElement("div");
         box.className = "box";
         box.setAttribute('id', `b-[${i}][${j}]`)
         box.textContent = GameBoard.gameArr[i][j]
         if (GameBoard.gameArr[i][j] == " ") {
-            box.addEventListener('click', (e) => emma.makeMove(e)) }
+            box.addEventListener('click', (e) => makeMove(e, player)) }
         row.appendChild(box);
     }
 }
@@ -54,8 +86,4 @@ for (let i = 0; i < GameBoard.gameArr.length; i ++) {
 
 document.addEventListener('DOMContentLoaded', addGrid() ) 
 
-//create a box div for each space on the game board array, each one with an event listener and an i/j value 
 
-//players take it in turn to make a move, a boolean denotes whether it is players turn which can be set to false
-//after a move is made
-//every move a player can take will have an i/j value and this can be pushed to game array
